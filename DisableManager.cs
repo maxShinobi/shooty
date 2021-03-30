a06af0c2dd8ef9e426a9adee35907c2b89efe7c5
Max Shinobi <mxshnb@gmail.com>
2021-03-30 18:55:56 +0900

    disable  manager when interacting

---
+++ DisableManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+namespace interactSystem
+{
+    public class DisableManager : MonoBehaviour
+    {
+        public static DisableManager instance;
+
+        [SerializeField] private FirstPersonController player;
+        [SerializeField] private Image crosshair; 
+
+        void Awake()
+        {
+            if (instance != null) { Destroy(gameObject); }
+            else { instance = this; DontDestroyOnLoad(gameObject); }
+        }
+
+        public void DisablePlayer(bool disable)
+        {
+            if (disable)
+            {
+                player.enabled = false;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                crosshair.enabled = false;
+            }
+
+            if (!disable)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                player.enabled = true;
+                crosshair.enabled = true;
+            }
+        }
+    }
+}
