15779dfc18409e025654be50b6c6a76bb14b85a4
Max Shinobi <mxshnb@gmail.com>
2021-03-31 22:07:36 +0900

    Player camera/fpc scripts merging, cleaning, fixing

--- Interactables/InteractionSystem.cs
+++ Interactables/InteractionSystem.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 using VHS;
 
 public class InteractionSystem : MonoBehaviour
@@ -9,11 +10,13 @@ public class InteractionSystem : MonoBehaviour
     [SerializeField] private new Camera camera = null;
     [SerializeField] RectTransform pickupImage = null;
     [SerializeField] private Image crosshair = null;
+    [SerializeField] private GameObject interactCanvas = null;
+    [SerializeField] private GameObject cameraHolder = null;
 
     public LayerMask layerMask;
 
     private Item itemBeingPickedUp;
-    [SerializeField] private CharacterController firstPersonController = null;
+    [SerializeField] private FirstPersonController firstPersonController = null;
     private 
 
     void Update()
@@ -24,11 +27,11 @@ public class InteractionSystem : MonoBehaviour
         {
             if(Input.GetButton("Fire1"))
             {
-                firstPersonController.enabled = false;
+                ShowInsideOfItem();
             }
         } else
         {
-
+            
         }
     }
 
@@ -62,5 +65,14 @@ public class InteractionSystem : MonoBehaviour
             pickupImage.gameObject.SetActive(false);
             crosshair.color = Color.white;
         }
+    }
+
+    private void ShowInsideOfItem()
+    {
+        firstPersonController.enabled = false;
+        Cursor.visible = true;
+        crosshair.enabled = false;
+        interactCanvas.SetActive(false);
+        cameraHolder.GetComponent<CameraController>().enabled = false;
     }
 }
